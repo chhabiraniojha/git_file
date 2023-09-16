85298ddc7c3ff79af34484711919d43e94662762
chhabiraniojha <chhabiraniojha@gmail.com>
2023-09-16 22:17:51 +0530

    delete button with functionality and with removal in localStorage added

--- appointment.js
+++ appointment.js
@@ -1,6 +1,7 @@
 const form=document.getElementById("form_element");
 const listItem=document.getElementById("item");
 form.addEventListener("submit",onSubmit);
+listItem.addEventListener("click",removeItem);
 
 
 
@@ -10,8 +11,13 @@ function onSubmit(e){
     const email=document.getElementById("floatingInput").value;
     const phone=document.getElementById("phone").value;
     const li=document.createElement("li");
-    li.appendChild(document.createTextNode(name+" "+email+" "+phone));
+    const deleteBtn=document.createElement("button")
+    deleteBtn.id="delete";
+    deleteBtn.appendChild(document.createTextNode("delete"))
+    li.appendChild(document.createTextNode(email));
+    li.appendChild(document.createTextNode(" - "+phone));
     listItem.appendChild(li);
+    li.appendChild(deleteBtn)
     const details=email;
     const userDetails={
         name:name,
@@ -21,4 +27,20 @@ function onSubmit(e){
     }
     const userdetails_string=JSON.stringify(userDetails);
     localStorage.setItem(details,userdetails_string);
+    console.log(li)
+    console.log(li.firstChild.textContent)
+    
+}
+
+
+function removeItem(e){
+    if(e.target.matches('#delete')){
+        if(confirm("are you sere")){
+            const li = e.target.parentElement;
+            listItem.removeChild(li);
+            localStorage.removeItem(li.firstChild.textContent)
+        }
+    }
+    
+
 }
